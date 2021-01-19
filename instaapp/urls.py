61d4eefd0c5fcb2004a9d58566bc4ffa700d770d
Marah-uwase <maranahuwase12@gmail.com>
2021-01-19 10:51:12 +0200

    configuring the url for the activation

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     
     url(r'^home/$', views.home, name='home'),
     url(r'^$',views.signup, name='signup'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+    url(r'^activat/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
     url(r'^user/(?P<username>\w{0,50})',views.profile,name='profile'),
     url(r'^upload/$', views.upload_image, name='upload_image'),
     url(r'^accounts/edit/',views.edit_profile, name='edit_profile'),
