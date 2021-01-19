fcdc379674e1daff16149c2ef03c89f214bea35a
Marah-uwase <maranahuwase12@gmail.com>
2021-01-19 11:57:04 +0200

    initializing the email file

--- instaapp/email.py
+++ instaapp/email.py
@@ -1,7 +1,7 @@
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_text
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_
 
 
 def send_activation_email(user, current_site, receiver):
