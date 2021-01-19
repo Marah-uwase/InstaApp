bdf23bd902108e94d33c69822103a154ec883ed4
Marah-uwase <maranahuwase12@gmail.com>
2021-01-19 10:50:04 +0200

    configuring the url

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
  # So we can use it like: {% url 'mymodule:user_register' %} on our template. 
 urlpatterns = [
     
-    url(r'^home/$', views.home, name='home'),
+    url(r'^home/$', views.home, name='home')
     url(r'^$',views.signup, name='signup'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
     url(r'^user/(?P<username>\w{0,50})',views.profile,name='profile'),
