6c73d9f43f5b510b6fd2c9eaa9c960d422ebdb82
Marah-uwase <maranahuwase12@gmail.com>
2021-01-19 10:56:47 +0200

    creating updating the caption

--- instaapp/models.py
+++ instaapp/models.py
@@ -5,7 +5,7 @@ from tinymce.models import HTMLField
 
 
 # Create your models here.
-class Image(models.Model)
+class Image(models.Model):
     image=models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length =30)
     caption = models.TextField(max_length =30)
